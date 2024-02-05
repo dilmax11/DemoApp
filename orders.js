405ea0c46c2613867b98a3da6f94607cb6d59c4f
dilmax11 <91269158+dilmax11@users.noreply.github.com>
2024-02-05 10:46:42 +0500

    yanvar ishlab chiqarish bo'yicha o'zgartirildi

---
+++ orders.js
@@ -0,0 +1,26 @@
+const Orders = [
+  {
+    productName: "1-Liniya",
+    productNumber: "49294",
+    paymentStatus: "Due",
+    status: `Activ`,
+  },
+  {
+    productName: "2-LIniya",
+    productNumber: "3187",
+    paymentStatus: "Refunded",
+    status: `Declined`,
+  },
+  {
+    productName: "3-Liniya",
+    productNumber: "49801",
+    paymentStatus: "Paid",
+    status: `Activ`,
+  },
+  {
+    productName: "0.53-Liniya",
+    productNumber: "3222",
+    paymentStatus: "Paid",
+    status: `Pending`,
+  },
+];
